3af03251f6ec4c9843ee31241c210e905bc7b8be
taoufik2020 <tabbabitawfic@gmail.com>
2020-10-31 21:06:30 +0100

    premier commit

---
+++ Controllers/userController.js
@@ -0,0 +1,3 @@
+exports.getUser = (req,res) => {
+    res.send('<h1 >hello les amis </h1>')
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const mongoose = require('mongoose')
+const userRoute = require('./router/user')
+
+app.use('/api/users',userRoute)
+/*mongoose.connect(process.env.DATABASE)
+.then(() => console.log("data base connected"))
+.catch(() => console.log('database not connected'))
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port,() => console.log("port run in port",port))
+
+
